8aa5f00b42b556cf6e52325f6b2b7a4aef00dff5
Tomás Franco <tomas.f.carvalho@gmail.com>
2019-07-24 23:10:32 -0300

    templates de email configurados

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '66c0c242706eb7',
+    pass: 'c7350796ad28bb',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com.br>',
+  },
+};
